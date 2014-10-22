369e1069c9caa16d01a5b210b4430f85a61a9005
Levi Truelove <levi.truelove@northpointeinc.com>
2014-10-22 08:38:38 -0400

    Added bootstrap CSS and cleaned up the index js by removing Sammy for the moment. It was giving lots of strange errors.

--- static/js/index.js
+++ static/js/index.js
@@ -18,16 +18,11 @@ function indexViewModel() {
             contentType: "application/json",
             dataType: "json",
             success: function(data, textStatus, request){
-                console.log(data);
                 self.passwordKey(null);
                 self.decryptedPasswords(data);
             },
             error: function(request, textStatus, errorThrown){
                 alert("There was a problem fetching your passwords.");
-                /*
-                console.log(errorThrown);
-                console.log(textStatus);
-                */
             },
             beforeSend: function(request){
                 request.setRequestHeader("Token", self.token);
@@ -49,32 +44,19 @@ function indexViewModel() {
             success: function(data, textStatus, request){
                 self.token = request.getResponseHeader("Token");
                 self.user(null);
-                location.hash = '/home';
-                console.log(data);
                 self.loggedInUser(data);
             },
             error: function(request, textStatus, errorThrown){
                 alert("Username and password combination doesn't exist");
-                /*
-                console.log(errorThrown);
-                console.log(textStatus);
-                */
             }
         });
 
         self.logout = function(){
-            location.hash = '/';
             self.user({Username: "", Password: ""});
             self.decryptedPasswords(null);
             self.loggedInUser(null);
-            self.token(null);
+            self.token = null;
         }
-
-        Sammy(function() {
-            this.get('', function() {
-                this.app.runRoute('get', '#Login');
-            });
-        }).run();
     };
 }
 
